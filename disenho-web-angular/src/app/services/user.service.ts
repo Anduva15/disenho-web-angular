ecadd866444cbb4b8989305c567851ce12bd500a
anduva15 <aullova25@gmail.com>
2023-08-06 15:23:38 -0600

    Angular user crud setup completed. Api connection pending in order to test crud methods

---
+++ disenho-web-angular/src/app/services/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private apiUrl = 'http://localhost:8085/users'; 
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.apiUrl);
+  }
+
+  getOneUser(): Observable<User[]>{
+    return this.http.get<User[]>(this.apiUrl + ":id")
+  }
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl, user);
+  }
+
+  updateUser(id: string, user: User): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl + ":id"}/${id}`, user);
+  }
+
+  deleteUser(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl + ":id"}/${id}`);
+  }
+}
--- disenho-web-angular/src/app/user/user.component.ts
+++ disenho-web-angular/src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from '../interfaces/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
+  user: User[] = [];
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.userService.getUsers().subscribe((user) => {
+      this.user = user;
+      console.log(user)
+    });
+  }
+
+  createUser(user: User) {
+    this.userService.createUser(user).subscribe((newUser) => {
+      this.user.push(newUser);
+    });
+  }
+
+  updateUser(id: string, user: User) {
+    this.userService.updateUser(id, user).subscribe((updatedUser) => {
+      const index = this.user.findIndex((u) => u._id === updatedUser._id);
+      if (index !== -1) {
+        this.user[index] = updatedUser;
+      }
+    });
+  }
+
+  deleteUser(id: string) {
+    this.userService.deleteUser(id).subscribe(() => {
+      this.user = this.user.filter((user) => user._id !== id);
+    });
+  }
 }
