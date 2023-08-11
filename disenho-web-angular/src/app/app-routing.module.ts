19bc0eadb86582384d0f365d1e8b838836399dbc
Nicolas_BL <nicolasbeisswenger@hotmail.com>
2023-08-11 13:56:51 -0600

    Manejo de errores 404 y 500

--- disenho-web-angular/src/app/app-routing.module.ts
+++ disenho-web-angular/src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { UserListComponent } from './user/user-list/user-list.component';
 import { UserFormComponent } from './user/user-form/user-form.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { InternalServerErrorComponent } from './internal-server-error/internal-server-error.component';
+
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
   { path: 'user/:id', component: UserDetailComponent },
   { path: 'user/add', component: UserFormComponent },
   { path: 'user/edit/:id', component: UserFormComponent },
   { path: '', redirectTo: '/users', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }, // Ruta para manejar errores 404
+  { path: 'server-error', component: InternalServerErrorComponent }, // Ruta para manejar errores 500
 ];
 
 @NgModule({
