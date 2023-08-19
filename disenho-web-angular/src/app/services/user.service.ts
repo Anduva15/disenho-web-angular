b2869bb3be9f0acb101911ac68c94f93a6239457
anduva15 <aullova25@gmail.com>
2023-08-19 13:06:50 -0600

    Encriptacion de password hecha

--- disenho-web-angular/src/app/services/user.service.ts
+++ disenho-web-angular/src/app/services/user.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { User } from '../interfaces/user';
 import { BaseService } from './BaseService';
 import { USERS, USER } from '../constants';
+import { Observable } from 'rxjs';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,5 +12,38 @@ import { USERS, USER } from '../constants';
 export class UserService extends BaseService<User> {
   constructor(http: HttpClient) {
     super(http, USER, USERS);
+  }
+
+  override create(user: Omit<User, '_id'>): Observable<User> {
+    const saltRounds = 10;
+
+    return new Observable<User>(observer => {
+      bcrypt.genSalt(saltRounds, (err, salt) => {
+        if (err) {
+          observer.error(err);
+        } else {
+          bcrypt.hash(String(user.password), salt, (err, hash) => {
+            if (err) {
+              observer.error(err);
+            } else {
+              const userWithHashedPassword: User = {
+                ...user as User,
+                password: hash,
+              };
+
+              super.create(userWithHashedPassword).subscribe(
+                createdUser => {
+                  observer.next(createdUser);
+                  observer.complete();
+                },
+                error => {
+                  observer.error(error);
+                }
+              );
+            }
+          });
+        }
+      });
+    });
   }
 }
